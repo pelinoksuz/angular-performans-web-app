2d584db1d9106daa825843ea8684cad2f4ce9850
Pelin <pelin.oksuz@avl.com>
2025-12-01 22:57:51 +0300

    fix: sidebar UI

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  isExpanded = false;
+
+  toggleSidebar() {
+    this.isExpanded = !this.isExpanded;
+  }
 
 }
