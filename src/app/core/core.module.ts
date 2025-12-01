9379bfe97d1be1737341f14486047d9fd3d4376a
Pelin <pelin.oksuz@avl.com>
2025-12-01 22:35:39 +0300

    feat: adding mat icons to the app

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MatButtonModule } from '@angular/material/button';
 import { SidebarComponent } from './sidebar/sidebar.component';
-
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [HeaderComponent, SidebarComponent],
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule,MatIconModule ],
   exports: [HeaderComponent, SidebarComponent]
 })
 export class CoreModule {}
