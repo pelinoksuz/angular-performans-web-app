f90f70725de58f0c7d1cafc1480799704779289b
Pelin <49640942+pelinoksuz@users.noreply.github.com>
2026-01-05 21:41:36 +0300

    feat: dark-light thema

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from './core/services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,12 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'frontend-bootcamp-dashboard-v2';
+export class AppComponent implements OnInit {
+  title = 'frontend-bootcamp-dashboard';
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.initTheme();
+  }
 }
